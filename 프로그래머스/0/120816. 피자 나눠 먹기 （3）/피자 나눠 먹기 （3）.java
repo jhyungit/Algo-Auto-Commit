4f731659df0e7d732aeed748e64b7aaecf96e124
Jeonghyun Lee <92933743+jhyungit@users.noreply.github.com>
2026-05-08 08:57:17 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.21 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int solution(int slice, int n) {
+        int answer = 0;
+        answer = (int) Math.ceil((double)n / slice);
+        return answer;
+    }
+}
