cc81c9ea4700026857a7c6fb1d1a050fd42d4a01
Jeonghyun Lee <92933743+jhyungit@users.noreply.github.com>
2026-05-10 15:36:42 +0900

    Refactor yellow signal detection logic

--- Python3/프로그래머스/1/468371. 노란불 신호등/노란불 신호등.py
+++ Python3/프로그래머스/1/468371. 노란불 신호등/노란불 신호등.py
@@ -1,23 +1,23 @@
+from math import gcd # 최대공약수
+from functools import reduce # 누적계산
+
+# 두 수의 최소공배수 구하기
+def lcm(a,b):
+    return a * b // gcd(a,b)
+
+def is_all_yellow(lights, time):
+    return all(light[(time - 1) % len(light)] == 'Y' for light in lights)
+
 def solution(signals):
-    answer = 0
-    light = []
+    lights = ['G'*g + 'Y'*y + 'R'*r for g,y,r in signals] # ['GGYRR', 'GGGGGYR'] 형태
+    periods = [len(light) for light in lights] # 각 주기
     
-    for signal in signals:
-        temp = ""
-        temp += 'G' * signal[0]
-        temp += 'Y' * signal[1]
-        temp += 'R' * signal[2]
-        temp *= 1_000_000
-        light.append(list(temp))
-        
+     # 모든 주기의 최소공배수
+    limit = reduce(lcm, periods)
+    
     time = 1
-
-    while time < 3_000_000:
-        check = set()
-        for l in light:
-            check.add(l[time])
-        time += 1
-        if check == set('Y'):
+    while time <= limit:
+        if is_all_yellow(lights, time):
             return time
-
+        time += 1
     return -1
